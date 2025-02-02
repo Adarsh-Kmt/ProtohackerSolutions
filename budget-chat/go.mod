e076f5d3c885a29b010ae6d824f84a59c29f5d34
Adarsh_Kmt <adarshkmt2004@gmail.com>
2025-02-02 13:03:33 +0530

    Initial implementation of budget chat.

---
+++ budget-chat/go.mod
@@ -0,0 +1,3 @@
+module github.com/adarsh-kmt/protohackersolutions/budget-chat
+
+go 1.23.3
---
+++ budget-chat/main.go
@@ -0,0 +1,221 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log/slog"
+	"net"
+	"sync"
+)
+
+const (
+	welcomeMessageFormat     = "Welcome to budget-chat! what do we call you?"
+	userMessageFormat        = "[%s] %s"
+	userJoinedMessageFormat  = "* %s has joined the chat."
+	onlineUsersMessageFormat = "* online users: "
+)
+
+type BudgetChat struct {
+	clients      map[string]net.Conn
+	clientsMutex *sync.RWMutex
+
+	connMutexMap map[string]*sync.Mutex
+}
+
+func NewBudgetChat() *BudgetChat {
+
+	return &BudgetChat{
+		clients:      make(map[string]net.Conn),
+		clientsMutex: &sync.RWMutex{},
+		connMutexMap: make(map[string]*sync.Mutex),
+	}
+}
+func validateName(name string) bool {
+
+	if len(name) == 0 {
+		return false
+	}
+	for _, ch := range name {
+		if !((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z') || (ch >= '0' && ch <= '9')) {
+			return false
+		}
+	}
+	return true
+}
+
+func (chat *BudgetChat) setName(conn net.Conn) (name string, err error) {
+
+	reader := bufio.NewReader(conn)
+
+	name, err = reader.ReadString('\n')
+	if err != nil {
+		slog.Error(err.Error(), "msg", "error while reading client's name")
+		return "", err
+	}
+	if chat.clients[name] != nil {
+		slog.Error("user with name " + name + " already exists.")
+		return "", fmt.Errorf("user with name " + name + " already exists.")
+	}
+	if !validateName(name) {
+		slog.Error("user with name " + name + " is not a valid name.")
+		return "", fmt.Errorf("user with name " + name + " is not a valid name.")
+	}
+
+	chat.clientsMutex.Lock()
+
+	chat.clients[name] = conn
+	chat.connMutexMap[name] = &sync.Mutex{}
+
+	chat.clientsMutex.Unlock()
+
+	return name, nil
+}
+
+func (chat *BudgetChat) broadcastData(aboutUser string, data []byte) error {
+
+	chat.clientsMutex.RLock()
+	defer chat.clientsMutex.RUnlock()
+
+	for username, conn := range chat.clients {
+
+		mutex := chat.connMutexMap[username]
+		mutex.Lock()
+		if username != aboutUser {
+
+			if _, err := conn.Write(data); err != nil {
+				slog.Error(err.Error(), "sender-username", aboutUser, "username", username, "msg", "error while sending message "+string(data))
+				return err
+			}
+		}
+		mutex.Unlock()
+	}
+
+	return nil
+}
+func (chat *BudgetChat) broadcastMessage(senderUsername string, message string) error {
+
+	message = fmt.Sprintf(userMessageFormat, senderUsername, message)
+
+	return chat.broadcastData(senderUsername, []byte(message))
+
+}
+
+func (chat *BudgetChat) broadcastPresenceNotification(name string) error {
+
+	usersOnlineNotification := onlineUsersMessageFormat
+
+	userJoinedMessage := fmt.Sprintf(userJoinedMessageFormat, name)
+
+	chat.clientsMutex.RLock()
+
+	for username, _ := range chat.clients {
+		if username != name {
+			usersOnlineNotification = username + ", " + usersOnlineNotification
+		}
+	}
+
+	chat.clientsMutex.RUnlock()
+
+	if err := chat.broadcastData(name, []byte(userJoinedMessage)); err != nil {
+		return err
+	}
+
+	if err := chat.sendData(name, []byte(usersOnlineNotification)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (chat *BudgetChat) sendData(name string, data []byte) error {
+
+	chat.clientsMutex.RLock()
+
+	conn := chat.clients[name]
+
+	mutex := chat.connMutexMap[name]
+
+	chat.clientsMutex.RUnlock()
+
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	if _, err := conn.Write(data); err != nil {
+		return err
+	}
+
+	return nil
+}
+func (chat *BudgetChat) handleUserExit(name string) error {
+
+	chat.clientsMutex.Lock()
+
+	delete(chat.clients, name)
+	delete(chat.connMutexMap, name)
+
+	chat.clientsMutex.Unlock()
+
+	userExitMessage := "* " + name + " has left the chat."
+
+	if err := chat.broadcastData(name, []byte(userExitMessage)); err != nil {
+		return err
+	}
+
+	return nil
+}
+func (chat *BudgetChat) handleClient(conn net.Conn) {
+
+	defer conn.Close()
+
+	_, err := conn.Write([]byte(welcomeMessageFormat))
+	if err != nil {
+		slog.Error(err.Error(), "msg", "error while writing welcome message to client")
+		return
+	}
+
+	name, err := chat.setName(conn)
+
+	if err != nil {
+		return
+	}
+
+	if err := chat.broadcastPresenceNotification(name); err != nil {
+		slog.Error(err.Error(), "msg", "error while writing welcome message to client")
+		return
+	}
+
+	reader := bufio.NewReader(conn)
+
+	for {
+
+		message, err := reader.ReadString('\n')
+		if err != nil {
+			slog.Error(err.Error(), "username", name, "msg", "error while reading message from client.")
+			_ = chat.handleUserExit(name)
+			return
+		}
+
+		if err = chat.broadcastMessage(name, message); err != nil {
+			return
+		}
+	}
+
+}
+func main() {
+
+	chat := NewBudgetChat()
+
+	listener, err := net.Listen("tcp", "0.0.0.0:8080")
+
+	if err != nil {
+		slog.Error(err.Error(), "msg", "error while creating listener")
+		return
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			slog.Error(err.Error(), "msg", "error while accepting connection")
+			return
+		}
+		go chat.handleClient(conn)
+	}
+}
